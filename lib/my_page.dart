6e4e4f4091aec7ad15b1a1d1594474a0ee27a640
Kenta Wakasa <wakasa.kenta.pg@gmail.com>
2022-08-14 10:04:54 +0900

    first commit

---
+++ lib/my_page.dart
@@ -0,0 +1,70 @@
+import 'package:chat/main.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class MyPage extends StatelessWidget {
+  const MyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // 何度も FirebaseAuth.instance.currentUser! と書くのは大変です。
+    // そこで適当な変数名をつけた変数に一時的に値を格納して記述量を短くする場合あります。
+    final user = FirebaseAuth.instance.currentUser!;
+    return Scaffold(
+      appBar: AppBar(title: const Text('マイページ')),
+      body: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          children: [
+            // ユーザーアイコン画像
+            CircleAvatar(
+              backgroundImage: NetworkImage(user.photoURL!),
+              radius: 40,
+            ),
+            // ユーザー名
+            Text(
+              user.displayName!,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 16),
+
+            // 部分的に左寄せにしたい場合の書き方
+            Align(
+              alignment: Alignment.centerLeft,
+              // ユーザー ID
+              child: Text('ユーザーID：${user.uid}'),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              // 登録日
+              child: Text('登録日：${user.metadata.creationTime!}'),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () async {
+                // Google からサインアウト
+                await GoogleSignIn().signOut();
+                // Firebase からサインアウト
+                await FirebaseAuth.instance.signOut();
+                // SignInPage に遷移
+                // このページには戻れないようにします。
+                Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(builder: (context) {
+                    return const SignInPage();
+                  }),
+                  (route) => false,
+                );
+              },
+              child: const Text('サインアウト'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/post.dart
@@ -0,0 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Post {
+  Post({
+    required this.text,
+    required this.createdAt,
+    required this.posterName,
+    required this.posterImageUrl,
+    required this.posterId,
+    required this.reference,
+  });
+
+  factory Post.fromFirestore(DocumentSnapshot<Map<String, dynamic>> snapshot) {
+    final map = snapshot.data()!; // data() の中には Map 型のデータが入っています。
+    // data()! この ! 記号は nullable な型を non-nullable として扱うよ！ という意味です。
+    // data の中身はかならず入っているだろうという仮説のもと ! をつけています。
+    // map データが得られているのでここからはいつもと同じです。
+    return Post(
+      text: map['text'],
+      createdAt: map['createdAt'],
+      posterName: map['posterName'],
+      posterImageUrl: map['posterImageUrl'],
+      posterId: map['posterId'],
+      reference: snapshot.reference, // 注意。reference は map ではなく snapshot に入っています。
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'text': text,
+      'createdAt': createdAt,
+      'posterName': posterName,
+      'posterImageUrl': posterImageUrl,
+      'posterId': posterId,
+      // 'reference': reference, reference は field に含めなくてよい
+      // field に含めなくても DocumentSnapshot に reference が存在するため
+    };
+  }
+
+  /// 投稿文
+  final String text;
+
+  /// 投稿日時
+  final Timestamp createdAt;
+
+  /// 投稿者の名前
+  final String posterName;
+
+  /// 投稿者のアイコン画像URL
+  final String posterImageUrl;
+
+  /// 投稿者のユーザーID
+  final String posterId;
+
+  /// Firestoreのどこにデータが存在するかを表すpath情報
+  final DocumentReference reference;
+}
